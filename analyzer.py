1cdccfbc2afdafbc69a80607cd1fe01e2e4c12a8
Caleb Foust <cfoust@sqweebloid.com>
2018-11-25 11:32:36 -0800

    Better filtering of low margins

--- analyzer.py
+++ analyzer.py
@@ -9,21 +9,33 @@ for uuid, item in items:
     sell = item['sell_average']
     buy = item['buy_average']
 
-    margin = 0 if sell == 0 or buy == 0 else abs((buy - sell) / sell)
-    margin *= 100 # For percentage
+    coin_margin = (buy - sell)
+    percent_margin = 0 if sell == 0 or buy == 0 else abs(coin_margin / sell)
+    percent_margin *= 100 # For percentage
 
-    item['margin'] = margin
+    item['margin'] = percent_margin
 
     quantity = item['overall_quantity']
     average = item['overall_average']
     volume = 0 if average == 0 else quantity / average
 
+    score = percent_margin * volume
+
     # Filter out low-cost items because they usually have unsatisfactory
     # profit
-    score = 0 if average < 100 else margin * volume
+    if average < 100 or coin_margin < 2:
+        score = 0
+
     item['score'] = score
 
 items = sorted(items, key=lambda a: a[1]['score'])
 
 for uuid, item in items:
-    print(item['name'], item['margin'], item['overall_quantity'])
+    output = [
+        item['name'],
+        item['margin'],
+        item['buy_average'],
+        item['sell_average'],
+        item['overall_quantity']
+    ]
+    print('\t'.join([str(x) for x in output]))
