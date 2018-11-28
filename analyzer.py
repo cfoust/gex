20c66c040329cefdf4451da2ad2257477e706330
Caleb Foust <cfoust@sqweebloid.com>
2018-11-28 14:39:20 +0000

    More adjustments

--- analyzer.py
+++ analyzer.py
@@ -15,31 +15,35 @@ for uuid, item in items:
     sell_quantity = item['sell_quantity']
     average = item['overall_average']
 
-    coin_margin = (buy - sell)
-    percent_margin = 0 if sell == 0 or buy == 0 else abs(coin_margin / sell)
+    coin_margin = abs(buy - sell)
+    percent_margin = 0 if sell == 0 or buy == 0 else coin_margin / sell
     percent_margin *= 100 # For percentage
 
     item['coin_margin'] = coin_margin
     item['percent_margin'] = percent_margin
 
+    market_cap = total_quantity * average
     seller_ratio = 0 if buy_quantity == 0 else sell_quantity / buy_quantity
 
     # Make sure there are about equal buyers and sellers
-    adjusted_ratio = pow(math.e, -1 * (pow(seller_ratio - 1, 2) / (2 * pow(1, 2))))
-    desirable_volume = total_quantity / 8000 if total_quantity < 8000 else 1
+    adjusted_ratio = pow(math.e, -1 * (pow(seller_ratio - 1.2, 2) / (2 * pow(0.5, 2))))
 
-    score = percent_margin * adjusted_ratio * desirable_volume
+    score = percent_margin * total_quantity * adjusted_ratio
 
     # Filter out low-cost items because they usually have unsatisfactory
     # profit. Also, it's highly unlikely that margins greater than ~30%
     # are real.
-    if average < 100 or coin_margin < 5 or percent_margin > 30:
+    if average < 100 or coin_margin < 5 or percent_margin > 25:
         score = 0
 
     item['score'] = score
 
+items = [(uuid, item) for uuid, item in items if item['overall_average'] < 1100000000]
 items = sorted(items, key=lambda a: a[1]['score'])
 items = reversed(items)
+
+def round_volume(volume):
+    return str(round(volume / 1000, 1)) + 'k'
 
 items = [
     [
@@ -48,9 +52,10 @@ items = [
         item['sell_average'],
         round(item['percent_margin'], 2),
         item['coin_margin'],
-        item['overall_quantity'],
-        item['buy_quantity'],
-        item['sell_quantity'],
+        round_volume(item['overall_quantity']),
+        round_volume(item['buy_quantity']),
+        round_volume(item['sell_quantity']),
+        # round(item['score'], 2),
     ]
     for uuid, item in items
 ]
@@ -64,6 +69,7 @@ headers = [
     'Overall V',
     'Buy V',
     'Sell V',
+    'Score',
 ]
 
 print(tabulate(items, headers=headers))
