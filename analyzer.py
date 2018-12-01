7f0c4ef266226ab7c40103b135a3a07313bd9934
Caleb Foust <cfoust@sqweebloid.com>
2018-12-01 19:01:29 +0000

    Introduce ge margin but don't use it

--- analyzer.py
+++ analyzer.py
@@ -7,6 +7,29 @@ r = requests.get("https://rsbuddy.com/exchange/summary.json")
 items = r.json()
 items = list(items.items())
 
+# First collect RS pricing data from RuneLite's API.
+def get_prices(items):
+    """
+    Gets the exact GE price of items using RuneLite's API.
+    RSBuddy's prices are kind of unreliable but at least they estimate margins.
+    """
+    # Can't use requests' understanding of params because the API is dumb.
+    parameters = '&'.join(['id=%d' % item for item in items])
+    r = requests.get('https://api.runelite.net/runelite-1.5.2.1/item/price?' + parameters)
+    return r.json()
+
+BATCH_SIZE = 100
+for i in range(0, len(items), BATCH_SIZE):
+    subset = items[i:i + BATCH_SIZE]
+    ids = [item[1]['id'] for item in subset]
+    prices = get_prices(ids)
+    prices = {str(item['id']):item['price'] for item in prices}
+
+    for uuid, item in subset:
+        if not uuid in prices:
+            continue
+        item['ge_price'] = prices[uuid]
+
 for uuid, item in items:
     sell = item['sell_average']
     buy = item['buy_average']
@@ -14,6 +37,7 @@ for uuid, item in items:
     buy_quantity = item['buy_quantity']
     sell_quantity = item['sell_quantity']
     average = item['overall_average']
+    ge_price = item['ge_price'] if 'ge_price' in item else buy
 
     coin_margin = abs(buy - sell)
     percent_margin = 0 if sell == 0 or buy == 0 else coin_margin / sell
@@ -27,6 +51,9 @@ for uuid, item in items:
 
     # Make sure there are about equal buyers and sellers
     adjusted_ratio = pow(math.e, -1 * (pow(seller_ratio - 1.2, 2) / (2 * pow(0.5, 2))))
+
+    # How far the average of the margin range is from the GE price
+    margin_distance = (((ge_price / ((buy + sell) / 2)) -1) * 100) / 8 if buy > 0 and sell > 0 else 1
 
     score = percent_margin * total_quantity * adjusted_ratio
 
@@ -50,12 +77,11 @@ items = [
         item['name'][:20],
         item['buy_average'],
         item['sell_average'],
+        item['ge_price'] if 'ge_price' in item else 'N / A',
         round(item['percent_margin'], 2),
         item['coin_margin'],
-        round_volume(item['overall_quantity']),
         round_volume(item['buy_quantity']),
         round_volume(item['sell_quantity']),
-        # round(item['score'], 2),
     ]
     for uuid, item in items
 ]
@@ -64,9 +90,9 @@ headers = [
     'Name',
     'Buy',
     'Sell',
+    'GE price',
     '% Margin',
     'Gp Margin',
-    'Overall V',
     'Buy V',
     'Sell V',
     'Score',
